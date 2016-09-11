426c94f1f2031d49eb08ba0bc05e34d9883e08a7
Peter Toth <tothpeter08@gmail.com>
2016-09-11 22:24:42 +0200

    Add control flow examples and exercises

--- 12-control-flow.exs
+++ 12-control-flow.exs
@@ -32,4 +32,44 @@ defmodule FizzBuzz2 do
   end
 end
 
-IO.inspect FizzBuzz2.upto(30)
+# IO.inspect FizzBuzz2.upto(30)
+
+defmodule FizzBuzz3 do
+  def upto(n) do
+    1..n |> Enum.map(&do_upto(&1))
+  end
+
+  defp do_upto(current) do
+    case {rem(current, 3), rem(current, 5)} do
+      {0, 0} -> "FizzBuzz"
+      {_, 0} -> "Fizz"
+      {0, _} -> "Buzz"
+      {_, _} -> current
+    end
+  end
+end
+
+# IO.inspect FizzBuzz3.upto(30)
+
+
+# defmodule Users do
+#   dave = %{ name: "Dave", state: "TX", likes: "programming" }
+#
+#   case dave do
+#     %{state: some_state} = person ->
+#       IO.puts "#{person.name} lives in #{some_state}"
+#     _ ->
+#       IO.puts "No matches"
+#   end
+# end
+
+defmodule ControlFlow do
+  def ok!({:ok, data}), do: data
+  def ok!({_, reason}), do: raise "Error occurred: #{reason}"
+end
+
+file = ControlFlow.ok! File.open("test_file")
+IO.inspect file
+
+file = ControlFlow.ok! File.open("not_exist")
+IO.inspect file
