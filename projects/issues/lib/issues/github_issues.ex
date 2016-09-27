48a6fcd1002a8516f3597d49e787911b18de52ff
Peter Toth <tothpeter08@gmail.com>
2016-09-27 10:46:17 +0200

    Decode json string

--- projects/issues/lib/issues/github_issues.ex
+++ projects/issues/lib/issues/github_issues.ex
@@ -5,7 +5,6 @@ defmodule Issues.GithubIssues do
   def fetch(user, project) do
     issues_url(user, project)
     |> HTTPoison.get!(@user_agent)
-    |> IO.inspect
     |> handle_response
   end
 
@@ -13,6 +12,11 @@ defmodule Issues.GithubIssues do
     "#{@github_url}/repos/#{user}/#{project}/issues"
   end
 
-  def handle_response(%{status_code: 200, body: body}), do: { :ok,    body }
-  def handle_response(%{status_code: ___, body: body}), do: { :error, body }
+  def handle_response(%{status_code: 200, body: body}) do
+    { :ok, :jsx.decode(body) }
+  end
+
+  def handle_response(%{status_code: ___, body: body}) do
+    { :error, :jsx.decode(body) }
+  end
 end
