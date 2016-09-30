4d5cde260f3940c87b8a7e893a3bd6041921c60b
Peter Toth <tothpeter08@gmail.com>
2016-09-30 12:41:53 +0200

    Pass the missing params to format fn

--- projects/issues/lib/issues/cli.ex
+++ projects/issues/lib/issues/cli.ex
@@ -45,7 +45,7 @@ defmodule Issues.CLI do
     |> convert_to_list_of_maps
     |> sort_into_ascending_order
     |> Enum.take(count)
-    |> Issues.TableFormatter.format
+    |> Issues.TableFormatter.format(["id", "created_at", "title"])
   end
 
   def decode_response({:ok, body}) do
