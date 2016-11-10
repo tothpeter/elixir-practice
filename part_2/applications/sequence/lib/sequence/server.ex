057a71787c0110106caf1ee04a6c98479b7f6d67
Peter Toth <tothpeter08@gmail.com>
2016-11-10 14:23:47 +0100

    One-Way Calls and fixes

--- part_2/applications/sequence/lib/sequence/server.ex
+++ part_2/applications/sequence/lib/sequence/server.ex
@@ -5,11 +5,22 @@ defmodule Sequence.Server do
     { :reply, current_number, current_number + 1 }
   end
 
-  def handle_call({:set_number, new_number}, _from, current_number) do
+  def handle_call({:set_number, new_number}, _from, _current_number) do
     { :reply, new_number, new_number }
   end
 
-  def handle_call(_any, _from, current_number) do
-    { :reply, {:error, "Dunno what to do"}, current_number + 1 }
+  def handle_call(_anything, _from, current_number) do
+    { :reply, {:error, "Dunno what to do"}, current_number }
+  end
+
+  def handle_cast({:increment, delta}, current_number) do
+    { :noreply, current_number + delta }
   end
 end
+
+# To test
+# r Sequence.Server
+# { :ok, pid } = GenServer.start_link(Sequence.Server, 100)
+# GenServer.call(pid, :next_number)
+# GenServer.cast(pid, {:increment, 10})
+# GenServer.call(pid, {:set_number, 10})
