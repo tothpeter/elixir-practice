543e86848713785f0cb659598e493cc558c76509
Peter Toth <tothpeter08@gmail.com>
2016-11-13 15:35:59 +0100

    Set initial value in mid.exs

--- part_2/applications/sequence/lib/sequence.ex
+++ part_2/applications/sequence/lib/sequence.ex
@@ -3,7 +3,7 @@ defmodule Sequence do
 
   # See http://elixir-lang.org/docs/stable/elixir/Application.html
   # for more information on OTP Applications
-  def start(_type, _args) do
-    {:ok,_pid} = Sequence.Supervisor.start_link(123)
+  def start(_type, initial_number) do
+    {:ok, _pid} = Sequence.Supervisor.start_link(initial_number)
   end
 end
