ef7c9068b464b7613f320c3a154a24d8ec0cf2ac
Peter Toth <tothpeter08@gmail.com>
2016-11-13 16:14:23 +0100

    Store initial value in envorinment

--- part_2/applications/sequence/lib/sequence.ex
+++ part_2/applications/sequence/lib/sequence.ex
@@ -3,7 +3,8 @@ defmodule Sequence do
 
   # See http://elixir-lang.org/docs/stable/elixir/Application.html
   # for more information on OTP Applications
-  def start(_type, initial_number) do
+  def start(_type, _params) do
+    initial_number = Application.get_env(:sequence, :initial_number)
     {:ok, _pid} = Sequence.Supervisor.start_link(initial_number)
   end
 end
--- part_2/applications/sequence/mix.exs
+++ part_2/applications/sequence/mix.exs
@@ -16,7 +16,8 @@ defmodule Sequence.Mixfile do
   def application do
     [
       applications: [:logger],
-      mod: { Sequence, 10 },
+      mod: { Sequence, [] },
+      env: [initial_number: 456],
       registered: [ Sequence.Server ]
     ]
   end
